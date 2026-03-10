665f109fd492135dc0e224c372e94995f55de62c
Tim Molteno <tim@molteno.net>
2026-03-11 09:23:32 +1300

    Add verify

--- src/tart_spi/programme.sh
+++ src/tart_spi/programme.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-openFPGALoader --cable ft2232_b --fpga-part xc6slx9tqg144 --write-flash firmware/tart.bit
+openFPGALoader --cable ft2232_b --fpga-part xc6slx9tqg144 --verify --write-flash firmware/tart.bit
